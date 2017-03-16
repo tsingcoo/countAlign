39a2a67ac5277a765fc77298070fbefe29dbe1f4
tsingcoo <tsingcoo.wang@gmail.com>
2017-03-16 11:19:00 +0800

    完成了测试文件中的对齐正确个数统计，接下来需要做的是计算正确率以及统计这些词在训练集中出现的频次，做成一张表

--- main.py
+++ main.py
@@ -1,39 +1,50 @@
 # coding:utf-8
 def getWordDict(corpus):
     wordDict = {}
-    wordVec = []
     with open(corpus) as f:
         for line in f.readlines():
             words = line.strip().split(' ')  # 现在得到的word是词的列表
-            wordVec.append(words)
             for word in words:
                 if word in wordDict:
                     wordDict[word] += 1
                 else:
                     wordDict[word] = 1
-    return wordDict, wordVec
+    return wordDict
 
 
-def countAlign(corpus, resultFile, referenceFile, wordDict):
+def getAlignDict(corpus, resultFile, referenceFile, wordDict):
     alignDict = wordDict  # 这个最好把alignDict里面的值初始化为0
-    for k, v in alignDict:
-        v = 0
+    for k in alignDict:
+        alignDict[k] = 0
 
-    with open(resultFile) as f1, open(corpus) as fc:
-        for line1, linec in zip(f1.readlines(), fc.readlines()):
-            aligns = line1.strip().split(' ')
+    with open(resultFile) as f1, open(referenceFile) as f2, open(corpus) as fc:
+        for line1, line2, linec in zip(f1.readlines(), f2.readlines(), fc.readlines()):
+            aligns1 = line1.strip().split(' ')
+            aligns2 = line2.strip().split(' ')
             words = linec.strip().split(' ')
-            for i in range(1, 1 + len(aligns)):
-                for align in aligns:
-                    if '-' + 'i' in align:  # 这里搞错了一点，因为这样只保证找到了，但并不能保证找到的是对的，因此还需要跟reference的对齐进行比较
-                        alignDict[aligns[i - 1]] += 1
+            for i in range(1, 1 + len(aligns1)):
+                str_i = str(i)
+                for align1 in aligns1:
+                    if '-' + str_i in align1:  # 小短杠加序号在对齐结果中
+                        for align2 in aligns2:
+                            if align1 in align2:  # 在对齐结果中的找到的对齐在参考对齐文件中
+                                alignDict[words[i - 1]] += 1
+                                break
+                        break  # 加入break是为了防止重复计算，但是为什么会重复我也没有想明白
+
+    return alignDict
 
 
 def main():
-    wordDict, wordVec = getWordDict(
-        '/Users/wangqinglong/Library/Mobile Documents/com~apple~CloudDocs/词对齐实验/比较/corpus.en')
-    # print(wordDict)
-    print(wordVec)
+    wordDict = getWordDict(
+        '/Users/wangql/Library/Mobile Documents/com~apple~CloudDocs/WordAlign/Compare/corpus.en')
+    print(wordDict)
+    alignDict = getAlignDict(
+        '/Users/wangql/Library/Mobile Documents/com~apple~CloudDocs/WordAlign/Compare/corpus.en',
+        '/Users/wangql/Library/Mobile Documents/com~apple~CloudDocs/WordAlign/Compare/infer.16.align',
+        '/Users/wangql/Library/Mobile Documents/com~apple~CloudDocs/WordAlign/Compare/test.qin.align',
+        wordDict)
+    print(alignDict)
 
 
 if __name__ == "__main__":
